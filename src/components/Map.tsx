458c8e469aa0f1f6a77e0871cd59e2a55d665e5f
PaulPlo <paulcravayat@gmail.com>
2024-05-15 18:15:47 +0200

    fix scrollwheelzoom

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -13,14 +13,13 @@ const Map = ({ data }) => {
     })
     
     return (
-        <MapContainer className={styles.container} attributionControl={false} center={[46.227638, 2.213749]} zoom={6} minZoom={3} maxZoom={15} scrollWheelZoom={false} zoomControl={false} ref={mapRef}>
+        <MapContainer className={styles.container} attributionControl={false} center={[46.227638, 2.213749]} zoom={6} minZoom={3} maxZoom={15} scrollWheelZoom={true} zoomControl={false} ref={mapRef}>
             <TileLayer
                 attribution= '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             
             {data.map((item, index: number) => {
-                console.log(item)
                 return (
                     <Marker icon={markerIcon} eventHandlers={{ click: () => console.log('click'), mouseover: (event) => event.target.openPopup(), mouseout: (event) => event.target.closePopup()}} key={index} position={[item.latlng.lat, item.latlng.lng]}>
                         <Popup>
