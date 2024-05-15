32305b9b25fcf85343ef73306d375bcec1fe8312
PaulPlo <paulcravayat@gmail.com>
2024-05-15 18:02:13 +0200

    template init

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,11 +1,18 @@
 import { useRef, useEffect } from 'react'; // Ajout de React et useRef
 import { MapContainer, TileLayer, Marker, ZoomControl, Popup } from 'react-leaflet'
+
 import styles from './Map.module.scss'
 
 const Map = ({ data }) => {
     const mapRef = useRef();
+    
+    const markerIcon = L.icon({
+        iconUrl: '/public/map-pin.svg',
+        iconSize: [20, 30],
+        iconAnchor: [10, 15]
+    })
+    
     return (
-        
         <MapContainer className={styles.container} attributionControl={false} center={[46.227638, 2.213749]} zoom={6} minZoom={3} maxZoom={15} scrollWheelZoom={false} zoomControl={false} ref={mapRef}>
             <TileLayer
                 attribution= '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -15,9 +22,9 @@ const Map = ({ data }) => {
             {data.map((item, index: number) => {
                 console.log(item)
                 return (
-                    <Marker eventHandlers={{ click: () => console.log('click'), mouseover: (event) => event.target.openPopup(), mouseout: (event) => event.target.closePopup()}} key={index} position={[item.latlng.lat, item.latlng.lng]}>
+                    <Marker icon={markerIcon} eventHandlers={{ click: () => console.log('click'), mouseover: (event) => event.target.openPopup(), mouseout: (event) => event.target.closePopup()}} key={index} position={[item.latlng.lat, item.latlng.lng]}>
                         <Popup>
-                            <div>{item.ville}</div>
+                            <div>{item.name}</div>
                         </Popup>
                     </Marker>
                 )
---
+++ src/config.js
@@ -0,0 +1,10 @@
+const config = {
+    colors: {
+        primary: '#3e63ff',
+        secondary: '#C9362E',
+        white: '#ffffff'
+    },
+    gridMargin: '20px',
+}
+
+export default config
